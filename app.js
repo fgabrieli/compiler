981ef98bf3da967f96b72470566acedac92c249f
Fernando Gabrieli <fgabrieli@gmail.com>
2016-06-19 17:15:15 -0300

    Added type checking

--- app.js
+++ app.js
@@ -1,46 +1,6 @@
-var str = '2+3+8+9+15-10';
+var extend = require('extend');
 
-function isExpr(input, subtree) {
-    return (subtree && subtree.type == 'expr') || (input.match(/^\d+[\+|\-]\d+/) != null);
-}
-
-function parseExpr(input, node) {
-    var regex, match, digit1, operator, digit2;
-
-    var subtree = new TreeNode('expr');
-    
-    if (node) {
-        regex = /^([\+|-])(\d+)/;
-
-        match = input.match(regex);
-
-        operator = match[1];
-        digit2 = match[2];
-
-        subtree.nodes.push(node);
-    } else {
-        regex = /^(\d+)([\+|-])(\d+)/;
-
-        match = input.match(regex);
-
-        digit1 = match[1];
-        operator = match[2];
-        digit2 = match[3];
-
-        subtree.nodes.push(new TreeNode('digit', digit1));
-    }
-
-    subtree.nodes.push(new TreeNode('operator', operator));
-
-    subtree.nodes.push(new TreeNode('digit', digit2));
-
-    var parseResult = {
-        subtree : subtree,
-        input : input.replace(regex, '')
-    }
-
-    return parseResult;
-}
+var parsers = [ new InfixParser() ];
 
 function TreeNode(type, data) {
     this.type = type ? type : '';
@@ -50,20 +10,89 @@ function TreeNode(type, data) {
     this.nodes = [];
 }
 
-function parse(input, subtree) {
-    var parsed = {};
+function Parser() {
 
-    if (isExpr(input, subtree)) {
-        parsed = parseExpr(input, subtree);
-
-        if (parsed.input.length > 0) {
-            return parse(parsed.input, parsed.subtree);
-        }
-    }
-
-    return parsed && parsed.subtree;
 }
 
-var parseTree = parse(str);
+/**
+ * Parse infix ops
+ */
+function InfixParser() {
+};
 
-console.log(parseTree);
+extend(InfixParser.prototype, Parser, {
+    parse : function(input, subtree) {
+        var parsed = {};
+
+        if (this.isExpr(input, subtree)) {
+            parsed = this.parseExpr(input, subtree);
+
+            if (parsed.input.length > 0) {
+                return this.parse(parsed.input, parsed.subtree);
+            }
+        }
+
+        return parsed.subtree || false;
+    },
+
+    isExpr : function(input, subtree) {
+        return (subtree && subtree.type == 'expr') || (input.match(/^\d+[\+|\-]\d+/) != null);
+    },
+
+    parseExpr : function(input, node) {
+        var regex, match, digit1, operator, digit2;
+
+        var subtree = new TreeNode('expr');
+
+        if (node) {
+            regex = /^([\+|-])(\d+)/;
+
+            match = input.match(regex);
+            if (match == null) {
+                var error = 'Syntax error at "' + input + '", invalid operator or non-digit used';
+                throw error;
+            }
+
+            operator = match[1];
+            digit2 = match[2];
+
+            subtree.nodes.push(node);
+        } else {
+            regex = /^(\d+)([\+|-])(\d+)/;
+
+            match = input.match(regex);
+
+            digit1 = match[1];
+            operator = match[2];
+            digit2 = match[3];
+
+            subtree.nodes.push(new TreeNode('digit', digit1));
+        }
+
+        subtree.nodes.push(new TreeNode('operator', operator));
+
+        subtree.nodes.push(new TreeNode('digit', digit2));
+
+        var parseResult = {
+            subtree : subtree,
+            input : input.replace(regex, '')
+        }
+
+        return parseResult;
+    }
+
+});
+
+// Main app
+
+{
+    var str = '2+3+8+9+15-10+4';
+
+    for (var i = 0; i < parsers.length; i++) {
+        var tree = parsers[i].parse(str);
+        if (tree) {
+            console.log(tree);
+            break;
+        }
+    }
+}
